a97da0c71da609a77eba652f8aa42322ae94c16d
orderlyapps <orderlyapps@gmail.com>
2026-05-09 00:28:56 +1000

    add back buttons to info, ui, and util page headers

--- src/content/pages/settings/info/info-header/InfoHeader.tsx
+++ src/content/pages/settings/info/info-header/InfoHeader.tsx
@@ -1,8 +1,11 @@
-import { IonToolbar, IonTitle } from "@ionic/react";
+import { IonToolbar, IonTitle, IonButtons, IonBackButton } from "@ionic/react";
 
 export function InfoHeader() {
   return (
     <IonToolbar>
+      <IonButtons slot="start">
+        <IonBackButton defaultHref="/settings" />
+      </IonButtons>
       <IonTitle>Info</IonTitle>
     </IonToolbar>
   );
--- src/content/pages/settings/info/util/util-header/UtilHeader.tsx
+++ src/content/pages/settings/info/util/util-header/UtilHeader.tsx
@@ -1,8 +1,11 @@
-import { IonToolbar, IonTitle } from "@ionic/react";
+import { IonToolbar, IonTitle, IonButtons, IonBackButton } from "@ionic/react";
 
 export function UtilHeader() {
   return (
     <IonToolbar>
+      <IonButtons slot="start">
+        <IonBackButton defaultHref="/settings/info" />
+      </IonButtons>
       <IonTitle>Util</IonTitle>
     </IonToolbar>
   );
