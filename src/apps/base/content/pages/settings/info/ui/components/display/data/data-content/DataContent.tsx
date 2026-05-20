3e17ede9fb79faf296df557c7bf09bb9737ec548
orderlyapps <orderlyapps@gmail.com>
2026-05-20 21:41:55 +1000

    add VerticalTextSection component to DataContent accordion group

--- src/apps/base/content/pages/settings/info/ui/components/display/data/data-content/DataContent.tsx
+++ src/apps/base/content/pages/settings/info/ui/components/display/data/data-content/DataContent.tsx
@@ -1,10 +1,12 @@
 import { LabelValueItemSection } from "@base-content/pages/settings/info/ui/components/display/data/label-value-item-section/LabelValueItemSection";
+import { VerticalTextSection } from "@base-content/pages/settings/info/ui/components/display/data/vertical-text-section/VerticalTextSection";
 import { IonAccordionGroup } from "@ionic/react";
 
 export function DataContent() {
   return (
     <IonAccordionGroup>
       <LabelValueItemSection />
+      <VerticalTextSection />
     </IonAccordionGroup>
   );
 }
