5743043c73747447f7cbfde7a77878f8f6542a90
orderlyapps <orderlyapps@gmail.com>
2026-05-28 08:54:58 +1000

    wrap AppearanceForm selectors in PageGrid component and refactor responsive CSS to use CSS custom properties with max-width container and configurable content width modifiers

--- src/apps/shared/appearance/AppearanceForm.tsx
+++ src/apps/shared/appearance/AppearanceForm.tsx
@@ -1,3 +1,4 @@
+import { PageGrid } from "@ui/components/layout/page-grid/PageGrid";
 import { useFontSize } from "@util/app/font-size/hooks/use-font-size";
 import { FontSizeSelector } from "@util/app/font-size/font-size-selector/FontSizeSelector";
 import { useTheme } from "@util/app/theme/hooks/use-theme";
@@ -8,9 +9,9 @@ export function AppearanceForm() {
   const { theme_mode, setTheme } = useTheme();
 
   return (
-    <>
+    <PageGrid cols={2}>
       <ThemeSelector value={theme_mode} onChange={setTheme} />
       <FontSizeSelector value={font_size} onChange={setFontSize} />
-    </>
+    </PageGrid>
   );
 }
