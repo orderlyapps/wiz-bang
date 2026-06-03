a4b5d15b2b04da1a3bace8fdf0e13f9b81638ae2
orderlyapps <orderlyapps@gmail.com>
2026-06-04 09:40:09 +1000

    add LogoIcon to HomeContent with centered layout, add ion-text-center class to Heading component

--- src/apps/proclaimer/content/pages/home/home-content/HomeContent.tsx
+++ src/apps/proclaimer/content/pages/home/home-content/HomeContent.tsx
@@ -1,9 +1,14 @@
+// import { LogoIcon } from "@shared/icons/logo";
+import { LogoIcon } from "@shared/icons/logo/LogoIcon";
 import { Heading } from "@ui/components/display/text/heading/Heading";
 
 export function HomeContent() {
   return (
     <>
-      <Heading size="2xl" bold>
+      <div className="ion-text-center ion-padding">
+        <LogoIcon size="2xl" color="primary" />
+      </div>
+      <Heading size="2xl" bold className="ion-text-center">
         Welcome to Proclaimer
       </Heading>
     </>
