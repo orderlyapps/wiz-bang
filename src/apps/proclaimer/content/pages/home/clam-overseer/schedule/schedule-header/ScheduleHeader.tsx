3207cde4800c86d8a76fd42f2ab43ca076a85a0d
orderlyapps <orderlyapps@gmail.com>
2026-06-08 21:11:27 +1000

    add IonBackButton to ScheduleHeader toolbar

--- src/apps/proclaimer/content/pages/home/clam-overseer/schedule/schedule-header/ScheduleHeader.tsx
+++ src/apps/proclaimer/content/pages/home/clam-overseer/schedule/schedule-header/ScheduleHeader.tsx
@@ -1,8 +1,11 @@
-import { IonToolbar, IonTitle } from "@ionic/react";
+import { IonToolbar, IonTitle, IonButtons, IonBackButton } from "@ionic/react";
 
 export function ScheduleHeader() {
   return (
     <IonToolbar>
+      <IonButtons>
+        <IonBackButton />
+      </IonButtons>
       <IonTitle>Schedule</IonTitle>
     </IonToolbar>
   );
