bcbf4bd5a7ad051507466586c3c5ce6bd4a10839
orderlyapps <orderlyapps@gmail.com>
2026-06-08 16:23:59 +1000

    add back button to Super Admin header

--- src/apps/proclaimer/content/pages/home/super-admin/super-admin-header/SuperAdminHeader.tsx
+++ src/apps/proclaimer/content/pages/home/super-admin/super-admin-header/SuperAdminHeader.tsx
@@ -1,8 +1,11 @@
-import { IonToolbar, IonTitle } from "@ionic/react";
+import { IonToolbar, IonTitle, IonButtons, IonBackButton } from "@ionic/react";
 
 export function SuperAdminHeader() {
   return (
     <IonToolbar>
+      <IonButtons slot="start">
+        <IonBackButton />
+      </IonButtons>
       <IonTitle>Super Admin</IonTitle>
     </IonToolbar>
   );
