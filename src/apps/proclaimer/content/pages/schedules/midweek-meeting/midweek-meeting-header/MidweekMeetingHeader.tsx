852c1ce5c6ceb76f30bec371a2436b9f2e802992
orderlyapps <orderlyapps@gmail.com>
2026-06-14 20:59:10 +1000

    add IonBackButton to MidweekMeetingHeader for navigation back to previous page

--- src/apps/proclaimer/content/pages/schedules/midweek-meeting/midweek-meeting-header/MidweekMeetingHeader.tsx
+++ src/apps/proclaimer/content/pages/schedules/midweek-meeting/midweek-meeting-header/MidweekMeetingHeader.tsx
@@ -1,8 +1,11 @@
-import { IonToolbar, IonTitle } from "@ionic/react";
+import { IonToolbar, IonTitle, IonButtons, IonBackButton } from "@ionic/react";
 
 export function MidweekMeetingHeader() {
   return (
     <IonToolbar>
+      <IonButtons slot="start">
+        <IonBackButton />
+      </IonButtons>
       <IonTitle>Midweek Meeting</IonTitle>
     </IonToolbar>
   );
