40275dcdf28f7e72352263568808fd911a756eca
orderlyapps <orderlyapps@gmail.com>
2026-06-25 09:18:40 +1000

    add unsaved changes alerts to MapMenu when editing blocks or changing maps, replace IonButton with TextButton/SaveTextButton components, sort blocks alphabetically by name, replace IonLabel text with Body component

--- src/apps/proclaimer/content/pages/home/service-overseer/service-overseer-map/components/map-menu/MapMenu.tsx
+++ src/apps/proclaimer/content/pages/home/service-overseer/service-overseer-map/components/map-menu/MapMenu.tsx
@@ -5,13 +5,17 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
-  IonButton,
   IonList,
   IonItem,
   IonLabel,
   IonButtons,
+  IonIcon,
 } from "@ionic/react";
+import { SaveTextButton } from "@ui/components/inputs/button/text/save/SaveTextButton";
+import { TextButton } from "@ui/components/inputs/button/text/TextButton";
 import { EditIconButton } from "@ui/components/inputs/button/icon/edit/EditIconButton";
+import { AlertIconButton } from "@ui/components/inputs/button/icon/alert/AlertIconButton";
+import { create } from "ionicons/icons";
 import { DeleteIconButton } from "@ui/components/inputs/button/icon/delete/DeleteIconButton";
 import { AddIconButton } from "@ui/components/inputs/button/icon/add/AddIconButton";
 import { AddBlockFlow } from "./components/add-block-flow/AddBlockFlow";
@@ -19,6 +23,7 @@ import type {
   Block,
   SelectedMap,
 } from "@proclaimer-content/pages/home/service-overseer/service-overseer-map/utils/types";
+import { Body } from "@ui/components/display/text/body/Body";
 
 type Props = {
   hasPendingChanges: boolean;
@@ -66,34 +71,61 @@ function MapMenu({
       />
       <IonContent className="content-wide">
         <IonList>
-          <IonItem>
-            <IonButton expand="block" disabled={!hasPendingChanges} onClick={onSave}>
-              Save Changes
-            </IonButton>
-          </IonItem>
-          {blocks?.map((block) => (
-            <IonItem key={block.id}>
-              <IonLabel>
-                <h3>{block.name}</h3>
-                <p>{block.type}</p>
-              </IonLabel>
-              <EditIconButton slot="end" on_click={() => onEditBlock(block)} />
-              <DeleteIconButton slot="end" on_click={() => onDeleteBlock(block.id)} />
-            </IonItem>
-          ))}
+          <TextButton label="Save Changes" disabled={!hasPendingChanges} on_click={onSave} />
+
+          {blocks
+            ?.sort((a, b) => a.name.localeCompare(b.name))
+            .map((block) => (
+              <IonItem key={block.id}>
+                <IonLabel>
+                  <Body>{block.name}</Body>
+                  <Body color="medium" size="xs">
+                    {" "}
+                    {block.type}
+                  </Body>
+                </IonLabel>
+
+                <IonButtons slot="end">
+                  {hasPendingChanges ? (
+                    <AlertIconButton
+                      alert_header="Unsaved Changes"
+                      alert_message="You have unsaved changes. Are you sure you want to edit this block?"
+                      confirm_text="Edit Block"
+                      on_click={() => onEditBlock(block)}
+                    >
+                      <IonIcon icon={create} />
+                    </AlertIconButton>
+                  ) : (
+                    <EditIconButton on_click={() => onEditBlock(block)} />
+                  )}
+                  <DeleteIconButton on_click={() => onDeleteBlock(block.id)} />
+                </IonButtons>
+              </IonItem>
+            ))}
         </IonList>
-        <IonItem>
-          <IonButton
-            expand="block"
+
+        {hasPendingChanges ? (
+          <SaveTextButton
+            label="Change Map"
             color="medium"
-            onClick={() => {
+            alert_header="Unsaved Changes"
+            alert_message="You have unsaved changes. Are you sure you want to change map?"
+            confirm_text="Change Map"
+            on_click={() => {
               void menu_ref.current?.close();
               onDeselect();
             }}
-          >
-            Change Map
-          </IonButton>
-        </IonItem>
+          />
+        ) : (
+          <TextButton
+            label="Change Map"
+            color="medium"
+            on_click={() => {
+              void menu_ref.current?.close();
+              onDeselect();
+            }}
+          />
+        )}
       </IonContent>
     </IonMenu>
   );
