b814bba3a49fb5b9a92571237c89825284c34862
orderlyapps <orderlyapps@gmail.com>
2026-06-25 17:55:52 +1000

    add IonBackButton to DoorToDoorHeader for navigation back to previous page

--- src/apps/proclaimer/content/pages/ministry/door-to-door/door-to-door-header/DoorToDoorHeader.tsx
+++ src/apps/proclaimer/content/pages/ministry/door-to-door/door-to-door-header/DoorToDoorHeader.tsx
@@ -1,8 +1,11 @@
-import { IonToolbar, IonTitle } from "@ionic/react";
+import { IonToolbar, IonTitle, IonButtons, IonBackButton } from "@ionic/react";
 
 export function DoorToDoorHeader() {
   return (
     <IonToolbar>
+      <IonButtons slot="start">
+        <IonBackButton />
+      </IonButtons>
       <IonTitle>Door To Door</IonTitle>
     </IonToolbar>
   );
