ef1a98c0fae3e8fa3077a7f35870b70286ced8df
orderlyapps <orderlyapps@gmail.com>
2026-06-30 05:50:06 +1000

    add ImportPublisherDataButton to SecretaryContent with file upload, confirmation alert, and import handling for publisher local data

--- src/apps/proclaimer/content/pages/home/secretary/secretary-content/SecretaryContent.tsx
+++ src/apps/proclaimer/content/pages/home/secretary/secretary-content/SecretaryContent.tsx
@@ -1,5 +1,6 @@
 import { IonList } from "@ionic/react";
 import { NavItem } from "@ui/components/navigation/nav-item/NavItem";
+import { ImportPublisherDataButton } from "./components/import-publisher-data-button/ImportPublisherDataButton";
 
 export function SecretaryContent() {
   return (
@@ -8,6 +9,7 @@ export function SecretaryContent() {
         <NavItem label="Publishers" to="/home/secretary/publishers" />
         <NavItem label="Groups" to="/home/secretary/groups" />
       </IonList>
+      <ImportPublisherDataButton />
     </>
   );
 }
