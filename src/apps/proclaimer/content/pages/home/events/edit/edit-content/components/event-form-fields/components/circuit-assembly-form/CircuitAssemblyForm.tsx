719764fa8598a92b5359014172c17767a876ae67
orderlyapps <orderlyapps@gmail.com>
2026-07-06 14:36:24 +1000

    add description Select field with Circuit Overseer/Branch Representative options to CircuitAssemblyForm, add placeholder text to name field, rename "Start Date" label to "Date", and remove end_date and address fields

--- src/apps/proclaimer/content/pages/home/events/edit/edit-content/components/event-form-fields/components/circuit-assembly-form/CircuitAssemblyForm.tsx
+++ src/apps/proclaimer/content/pages/home/events/edit/edit-content/components/event-form-fields/components/circuit-assembly-form/CircuitAssemblyForm.tsx
@@ -1,22 +1,35 @@
+import { Select } from "@ui/components/inputs/select/Select";
 import { TextInput } from "@ui/components/inputs/text/TextInput";
 import { DateInput } from "@ui/components/inputs/date/DateInput";
 import type { EventFormFieldProps } from "../../types";
+
+const DESCRIPTION_OPTIONS = [
+  { label: "with Circuit Overseer", value: "with Circuit Overseer" },
+  { label: "with Branch Representative", value: "with Branch Representative" },
+];
 
 export function CircuitAssemblyForm(props: EventFormFieldProps) {
   const { on_change } = props;
   return (
     <>
-      <TextInput label="Name" value={props.name} on_change={(v) => on_change("name", v)} />
-      <TextInput label="Address" value={props.address} on_change={(v) => on_change("address", v)} />
-      <DateInput
-        label="Start Date"
-        value={props.start_date}
-        on_change={(v) => on_change("start_date", v)}
+      <Select
+        label="Description"
+        value={props.description}
+        options={DESCRIPTION_OPTIONS}
+        placeholder="Select a description"
+        on_change={(v) => on_change("description", v as string)}
+        interface_type="popover"
+      />
+      <TextInput
+        label="Name"
+        value={props.name}
+        placeholder="TBC"
+        on_change={(v) => on_change("name", v)}
       />
       <DateInput
-        label="End Date"
-        value={props.end_date}
-        on_change={(v) => on_change("end_date", v)}
+        label="Date"
+        value={props.start_date}
+        on_change={(v) => on_change("start_date", v)}
       />
     </>
   );
