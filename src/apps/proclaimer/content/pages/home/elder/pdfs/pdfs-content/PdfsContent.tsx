3dd2988e2b26ed9fc1693bffcb884908a016f1b3
orderlyapps <orderlyapps@gmail.com>
2026-07-12 18:35:42 +1000

    fix capitalization of "Weekend Meeting" nav item label in PdfsContent

--- src/apps/proclaimer/content/pages/home/elder/pdfs/pdfs-content/PdfsContent.tsx
+++ src/apps/proclaimer/content/pages/home/elder/pdfs/pdfs-content/PdfsContent.tsx
@@ -7,7 +7,7 @@ export function PdfsContent() {
       <NavItem label="Contacts List" to="/home/elder/contacts-list" />
       <NavItem label="Groups" to="/home/elder/pdfs/groups" />
       <NavItem label="Midweek Meeting" to="/home/elder/clam" />
-      <NavItem label="weekend Meeting" to="/home/elder/pdfs/speaker-schedule" />
+      <NavItem label="Weekend Meeting" to="/home/elder/pdfs/speaker-schedule" />
       <NavItem label="Audio Video" to="/home/elder/audio-video" />
       <NavItem label="Cleaning" to="/home/elder/cleaning-schedule" />
     </IonList>
